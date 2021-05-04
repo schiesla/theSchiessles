90a3ca283b0cbffeab42dbec020c5d9b5ff1ba5e
schiesla <schizzla27@gmail.com>
2021-05-04 16:43:14 -0400

    more from last push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ImageCarousel from '../components/ImageCarousel';
 
 class Home extends React.Component {
     render() {
        return (
           <div>
-             <h1>Home...</h1>
+             home...
+          <ImageCarousel />
           </div>
        )
     }
